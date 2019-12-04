2ca656708d136dc394303541f1f6f2c64becaac6
alineapsil <alineapsil@yahoo.com>
2019-12-04 13:45:30 -0300

    Atualização Exercicios

--- exercicio-casa/Aline Silva/script.js
+++ exercicio-casa/Aline Silva/script.js
@@ -1,8 +1,17 @@
 // Inicie o jQuery no seu documento
-$(document).ready(function(){
-    
-}
-// Faça com que a lista de conteúdo apareça ao clicar em Confira
+$(document).ready(function() {
+  // Faça com que a lista de conteúdo apareça ao clicar em Confira
+
+  $(".subtitle").click(function() {
+    $(".product_list").toggle("slow");
+  });
+
+  product_item;
+
+  $(".product_item").click(function() {
+    $(".container").css("background", "");
+  });
+});
 
 // Ao clicar em cada um dos conteúdos, mude a cor de fundo do site. Se quiser, pode trabalhar com transparências usando o rgba.
 
@@ -11,6 +20,3 @@ $(document).ready(function(){
 // Ao clicar no botão, o formulário deve aparecer
 
 // Ao clicar em Enviar, o formulário deve desaparecer e uma mensagem de "Seu formulário foi enviado" deve aparecer na tela. Essa mensagem pode ser gerada dinamicamente em jQuery
-
-
-
