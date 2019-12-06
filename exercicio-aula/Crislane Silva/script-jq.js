5923b8c49e813e3869ab250119e2a6940335efc9
Crislane Silva <crislane.silva@gmail.com>
2019-12-05 23:58:34 -0300

    Exercicio Crislane Silva

---
+++ exercicio-aula/Crislane Silva/script-jq.js
@@ -0,0 +1,18 @@
+$(function () {
+
+    // $("h1").css("color", "purple");
+    // $("p").css("font-size", "34px");
+    // $("h1").css({"color": "green", "font-size": "24px"});
+    // $("p").addClass("texto-inicial"); 
+
+    // $(window).scroll(function(){
+    //     $('.product_list').fadeOut('1000');
+    // });
+
+
+    $(window).resize(function () {
+        $(".logo").width($(window).width()).height($(window).height())
+    })
+
+});
+
